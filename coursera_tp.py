bb4efc87d5f0e8dabfe13f13221c16f187f53635
tiornasoro <114851561+tiornasoro@users.noreply.github.com>
2022-10-06 08:37:39 +0100

    Add files via upload

---
+++ coursera_tp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# SORO TIORNA
+# I want to be a data scientist
+
+#  I am interested in data science because it's the present and futur business.
+
+# ### This code will calcute the discriminant of polynom 
+
+# In[2]:
+
+
+a=3
+b=4
+c=6
+dis=b*b-(4*a*c)
+print('the discriminant is:',dis)
+
+
+#  A data scientist must now:
+#      . statistics
+#      . informatics
+#      . mathmatics
+#      
+#  and can be from :
+#      1-mathmatics studies
+#      2-engineer studies
+#      3-computer sciences
+#      
+#  all of this  is on : https://www.coursera.org/
+
+# In[ ]:
+
+
+
+
